d5301c7b7bdddc0db216cfe79038f79214908146
Wojciech Kos <obohp@wp.pl>
2020-09-06 09:15:48 +0200

    Generated project task 28

---
+++ src/main/java/pl/wkos/homework28/Homework28Application.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework28Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wkos/homework28/Homework28ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
